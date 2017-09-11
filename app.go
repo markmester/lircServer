6fcf3ae95101597ca79f52a35dcf25ee3fb72482
admin <admin@quadrint.com>
2017-09-10 21:00:34 -0400

    added endpoint for vizio speaker control, added lirc configs

--- app.go
+++ app.go
@@ -8,10 +8,11 @@ import (
 	"github.com/gorilla/mux"
 	"os/exec"
 	"fmt"
+	"time"
 )
 
 func PowerEndpoint(w http.ResponseWriter, req *http.Request) {
-	fmt.Println(">>> Sending LIRC KEY_POWER command...")
+	fmt.Println(">>> Sending LIRC KEY_POWER command to roku remote...")
 	out := map[string]bool{
 		"success": true,
 	}
@@ -28,8 +29,59 @@ func PowerEndpoint(w http.ResponseWriter, req *http.Request) {
 	json.NewEncoder(w).Encode(out)
 }
 
+func PowerOffBluetoothEndpoint(w http.ResponseWriter, req *http.Request) {
+	fmt.Println(">>> Sending LIRC KEY_POWER command to vizio remote...")
+	out := map[string]bool{
+		"success": true,
+	}
+	cmd := "irsend"
+	args := []string{"SEND_ONCE", "count==2", "vizio", "KEY_POWER"}
+	lsCmd := exec.Command(cmd, args...)
+	_, err := lsCmd.Output()
+	if err != nil {
+		fmt.Println("Unable to run vizio KEY_POWER command; Error: ", err)
+		out["success"] = false
+	}
+
+	json.NewEncoder(w).Encode(out)
+}
+
+func ConnectToBluetoothEndpoint(w http.ResponseWriter, req *http.Request) {
+	fmt.Println(">>> Sending LIRC KEY_POWER command to vizio remote...")
+	out := map[string]bool{
+		"success": true,
+	}
+	cmd := "irsend"
+	args := []string{"SEND_ONCE", "count==2", "vizio", "KEY_POWER"}
+	lsCmd := exec.Command(cmd, args...)
+	_, err := lsCmd.Output()
+	if err != nil {
+		fmt.Println("Unable to run vizio KEY_POWER command; Error: ", err)
+		out["success"] = false
+	}
+
+	time.Sleep(3 * time.Second)
+
+	cmd = "irsend"
+	args = []string{"SEND_ONCE", "count==2", "vizio", "KEY_BLUE"}
+	lsCmd = exec.Command(cmd, args...)
+	_, err = lsCmd.Output()
+	if err != nil {
+		fmt.Println("Unable to run vizio KEY_BLUE command; Error: ", err)
+		out["success"] = false
+	}
+
+	json.NewEncoder(w).Encode(out)
+}
+
 func main() {
+	// instantiate server
 	router := mux.NewRouter()
+
+	// endpoints
 	router.HandleFunc("/power", PowerEndpoint).Methods("GET")
+	router.HandleFunc("/powerBluetooth", PowerOffBluetoothEndpoint).Methods("GET")
+	router.HandleFunc("/connectToBluetooth", ConnectToBluetoothEndpoint).Methods("GET")
+
 	log.Fatal(http.ListenAndServe(":5000", router))
 }
